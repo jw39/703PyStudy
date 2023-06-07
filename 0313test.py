8478d5c2947dc8048c76ef704cf1b792d6f68c7d
jiwon choi <109200207+jw39@users.noreply.github.com>
2023-06-07 16:56:37 +0900

    0607 수정
    주석 지움

--- 0313test.py
+++ 0313test.py
@@ -1,17 +1,16 @@
-""" x = input("첫번째 정수: ")
+x = input("첫번째 정수: ")
 y = input("두번째 정수: ")
-print(int(x)-int(y)) """
+print(int(x)-int(y)) 
 
-""" p = 3.14
+
+p = 3.14
 r= int(input("반지름을 입력하세요"))
 # z = 2*p*r
 # print(z)
 print(2*p*r)
- """
 
-""" 
-# p = 3.14
-# r = float(input("장어의 직경:"))
-# print("장어의 단면은 %f 입니다"%(p*r))
 
- """
+p = 3.14
+r = float(input("장어의 직경:"))
+print("장어의 단면은 %f 입니다"%(p*r))
+
